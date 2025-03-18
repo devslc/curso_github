8485df941cdc6b74380c867eb14388c3101f7df2
devslc <sheila.lagares.sl@gmail.com>
2025-03-18 17:37:26 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -1,4 +1,15 @@
 
 //Trabalhando no login
 
-// finalizando sistema de login
+// finalizando sistema de 
+
+
+function login (nome, email){
+    return 'Bem vindo';
+}
+
+
+//Seja Bem vindo
+
+//sistema de login completo
+//returnando o nome do usuario quando logado
